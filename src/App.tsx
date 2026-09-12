f9b936be3b5d97558bc5aed854b5a00feb5d2d56
Abu Tahir Sk <abutahir2626@gmail.com>
2026-09-12 19:31:31 +0530

    devstack tailwind css connect

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import '../src/components/Navbar'
+import Navbar from '../src/components/Navbar'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+      return (
+            <div>
+                  <h2 className="text-2xl font-bold text-red-500">navbar</h2>
+            </div>
+      );
+};
+
+export default Navbar;
